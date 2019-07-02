23c4296bf0a38e780b8c68dcd32ae49fa4fee7a2
Augusto <gutopssouza@gmail.com>
2019-07-01 21:24:49 -0300

    :rocket: Create User migration Create User model Create model Loader Set database config

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ message: 'Hello World' }));
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Augusto Pacheco',
+    email: 'gutopssouza@gmail.com',
+    password_hash: 'augusto2',
+  });
+
+  return res.json(user);
+});
 
 export default routes;
