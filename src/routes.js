e220266df30f9b5b13ce038ba6c4a0972212e1eb
Augusto <gutopssouza@gmail.com>
2019-07-01 21:43:01 -0300

    :rocket: Create UserController and user register api

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Augusto Pacheco',
-    email: 'gutopssouza@gmail.com',
-    password_hash: 'augusto2',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
