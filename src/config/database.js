5deeb7412fa4310e0d4b61491795188afdfe7ac0
Augusto <gutopssouza@gmail.com>
2019-07-01 21:05:15 -0300

    :rocket: Sequelize config

---
+++ src/config/database.js
@@ -0,0 +1,13 @@
+module.exports = {
+  dialect: 'postgres',
+  host: '192.168.99.100',
+  username: 'postgres',
+  password: 'docker',
+  database: 'gobarber',
+  port: '5433',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
