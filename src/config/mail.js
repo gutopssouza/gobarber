f722cbee5161a244a6c514e3923e11bb2815661b
Augusto <gutopssouza@gmail.com>
2019-07-16 20:59:05 -0300

    :rocket: Implement email service for cancelling an appointment using Nodemailer lib on src/config/mail src/lib/Mail src/app/controllers/AppointmentController

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '48d0d35ef50b06',
+    pass: 'b1c9e929bb6b7f',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
